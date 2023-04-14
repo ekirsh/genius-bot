74d60f1a48c99547f21e8c1dd0b7eef59092e1f3
Ezra Kirsh <ezkirsh@gmail.com>
2023-04-13 22:10:52 -0400

    fuck docker 17

--- newtrst.py
+++ newtrst.py
@@ -137,12 +137,12 @@ def get_top_songs(artist_name, driver):
 # Initialize the Chrome driver
 
 chrome_options = Options()
-ser = Service(ChromeDriverManager().install())
-chrome_options.add_argument("--headless=new")
+#ser = Service(ChromeDriverManager().install())
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--dns-prefetch-disable")
-chrome_options.add_argument("--disable-gpu")
+#chrome_options.add_argument("--no-sandbox")
+#chrome_options.add_argument("--dns-prefetch-disable")
+#chrome_options.add_argument("--disable-gpu")
 print(os.environ.get("CHROME_PATH"))
 #chrome_options.binary_location = os.environ.get("CHROME_PATH")
 driver = uc.Chrome(options=chrome_options) #, service=ser)
